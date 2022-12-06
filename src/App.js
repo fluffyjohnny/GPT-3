6eb7812e32b53d6c0be2380ec9ac77396728cca0
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-12-05 16:28:54 -0800

    import components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import { Article, Brand, CTA, Feature, Navbar } from './components'
+import React from "react";
+import { Brand, CTA, Navbar } from "./components";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
 
 const App = () => {
   return (
     <div>
       <h1>GPT-3</h1>
 
-
       <h1>GPT-3</h1>
       <h1>GPT-3</h1>
       <h1>GPT-3</h1>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
