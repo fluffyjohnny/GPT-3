05eddc66a7ce63a4ed9d4475e61dc990933d2d9d
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-12-06 11:04:00 -0800

    add css properties and classes

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -4,7 +4,7 @@ import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
   return (
-    <div>
+    <div className="gpt3__brand section__padding">
       <div>
         <img src={google} alt='google'/>
       </div>
